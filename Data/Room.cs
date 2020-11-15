6a670196d007f672a5fa9641f88197b5f9c42b0a
eswong610 <eswong610@gmail.com>
2020-11-14 19:09:49 -0800

    first commit

---
+++ Data/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.Data
+{
+    class Room
+    {
+        public int RoomID { get; set; }
+        public int LocationID { get; set; }
+        public int RoomTypeID { get; set; }
+        public int BookingID { get; set; }
+    }
+}
